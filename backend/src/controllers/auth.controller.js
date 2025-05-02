92e318dba19ffefc954b9de03234ccc2c9dcb709
khadon <nguyendinhdongkha@gmail.com>
2025-05-02 23:10:11 +0700

    auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
     const { email, fullName, password } = req.body;
     try {
+        //validate 
+        if (!email || !fullName || !password) return res.status(400).send("Password, fullName and email are required");
         //hash password
         if (password.length < 6) {
             return res.status(400).send("Password must be at least 6 characters")
@@ -29,8 +31,22 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error" })
     }
 }
-export const login = (req, res) => {
-    res.send("login")
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).send("Invalid email or password");
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).send("Invalid email or password");
+        }
+        generateToken(user._id, res);
+        res.status(200).json({ _id: user._id, email: user.email, fullName: user.fullName, profilePic: user.profilePic });
+    } catch (error) {
+        console.log("Error in login");
+        res.status(500).json({ message: "Internal server error" })
+    }
 }
 export const logout = (req, res) => {
     res.send("logout")
